4a1a3b8be90adf01f23f7345b8b047a88f0c5207
Sudhanshu Kumar <sudhanshusham57@gmail.com>
2022-10-01 13:58:05 +0530

    defaultValue

---
+++ 1/es6-feature-defaultvalueoffunction.js
@@ -0,0 +1,10 @@
+ // Default Parameters
+
+ var calculateArea = function(height=10,widht=20){
+    calculateArea = height * widht ;
+   return calculateArea; 
+   }
+   console.log(calculateArea(10, ));
+  
+  
+   //Here widht  is not defined , so it takes the default value . and calculate area based on that. 
