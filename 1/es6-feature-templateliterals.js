88322938d5f7e95475b9129bd16b5ebe710ecd46
Sudhanshu Kumar <sudhanshusham57@gmail.com>
2022-10-01 14:10:33 +0530

    templates

---
+++ 1/es6-feature-templateliterals.js
@@ -0,0 +1,10 @@
+//  Template Literals in ES6
+
+let firstName = 'sudhanshu';
+let lastName = 'kumar';
+
+var name = `Your name is ${firstName} ${lastName}.`
+
+console.log(name);
+
+// value of variable can be inserted in it with the help of the template literals by using backtick.
