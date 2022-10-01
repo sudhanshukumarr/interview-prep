2f6ef3e5b5ead5f67c86e46cb3fa00abd039e995
Sudhanshu Kumar <sudhanshusham57@gmail.com>
2022-10-01 14:46:27 +0530

    string

---
+++ 1/es6-feature-destructuring.js
@@ -0,0 +1,16 @@
+// destructring arrays
+
+var o =[3,4];
+var[p,q] = o;
+console.log(p);
+
+let arr = [1,2,3];
+let [x,y,z] =arr;
+console.log(x);
+
+//destructuring object
+
+let s = {a:1,b:2,c:3,d:4}
+let{a,b,c,d} = s;
+console.log(a);
+
---
+++ 1/es6-multilineString.js
@@ -0,0 +1,6 @@
+let poem = `johnny johnny yes papa,
+            eating sugar no papa!
+            telling lies? no papa!
+            open your mouth ah ,ah,ah!`;
+
+            console.log(poem);
